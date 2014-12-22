f3fa71c9c3b1f79457179ecb3fe38a55acb0478b
Rajesh Veeranki <rveeranki01@gmail.com>
2014-12-22 19:17:40 +0530

    Added variables

--- Handlers.py
+++ Handlers.py
@@ -2,17 +2,19 @@ import SimpleHTTPServer
 class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	requests = []
 	msg = "<html><body><p>Too many requests</p></body></html>"
+	MAX_SIZE = 20
+	MAX_REQ = 5
 
 	def do_GET(self):
 		client_ip = self.client_address[0]
 
-		if len(self.requests)<20:
+		if len(self.requests) < self.MAX_SIZE:
 			self.requests.append(client_ip)
 		else:
 			self.requests.pop(0)
 			self.requests.append(client_ip)
 
-		if self.requests.count(client_ip) > 5:
+		if self.requests.count(client_ip) > self.MAX_REQ:
 			self.send_response(429)
 			self.send_header("Content-type", "text/html")
 			self.end_headers()
